82b54046e9de82d440c1969181516dc680475382
arpa0118 <parfenens@gmail.com>
2018-07-30 17:22:54 +0300

    First commit

---
+++ src/main/java/com/pis/realestatemanagement/RealEstateManagementApplication.java
@@ -0,0 +1,12 @@
+package com.pis.realestatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateManagementApplication.class, args);
+    }
+}
